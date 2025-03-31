b5ddcfe161c491b3af09e08a4a3c65489e116c88
babubisleri001 <mishrashubham10639@gmail.com>
2025-03-31 19:13:43 +0530

    implement user interface components

---
+++ frontend/src/components/AuthImagePattern.jsx
@@ -0,0 +1,16 @@
+const AuthImagePattern = ({ image, title, subtitle }) => {
+    return (
+      <div className="hidden lg:flex flex-col items-center justify-center bg-base-200 p-12 text-center">
+        <div className="flex flex-col items-center justify-center mb-6 w-full">
+          {image}
+        </div>
+        <div className="max-w-md">
+          <h2 className="text-2xl font-bold mb-4">{title}</h2>
+          <p className="text-base-content/60">{subtitle}</p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default AuthImagePattern;
+  
